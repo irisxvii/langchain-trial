7952dda6a30b84a45b99cfce99c1f37e59b818c7
irisxvii <iriskurien@gmail.com>
2024-07-07 18:40:53 +0530

    initial commit

---
+++ yt-transcriber.py
@@ -0,0 +1,24 @@
+import os
+import openai
+import sys
+sys.path.append('../..')
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.environ['use_your_api_key']
+
+# -----yt audio transcription-----
+
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import OpenAIWhisperParser
+from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
+
+url="https://www.youtube.com/watch?v=jGwO_UgTS7I"
+save_dir="/home/user/Desktop/langchain"
+loader = GenericLoader(
+    YoutubeAudioLoader([url],save_dir),
+    OpenAIWhisperParser()
+)
+docs = loader.load()
+docs[0].page_content[0:500]
